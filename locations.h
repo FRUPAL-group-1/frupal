bcb752561924e28c12bf54080d7f42540269ec51
Gunnar R <gunnarrosenberg@gmail.com>
2020-11-17 23:49:33 -0800

    mostly done with locations class functions

--- locations.h
+++ locations.h
@@ -1,4 +1,19 @@
 // C. Gunnar Rosenberg
 // Locations
 
+#include <string.h>
+#include <map>
+#include <any>
+using namespace std;
+
+class Locations {
+  protected: 
+    map<string, any> grovnicks;
+
+  public:
+    any get(string key);
+    bool set(string key, any grovnick);	
+    void destroy(string key);
+
+};
 
