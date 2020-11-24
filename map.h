d958979608cc0a65e77a5516b434928cd3b1face
Gunnar R <gunnarrosenberg@gmail.com>
2020-11-23 20:47:12 -0800

    remove artifact from merge conflict

--- map.h
+++ map.h
@@ -1,17 +1,16 @@
 //map.h
+#include "locations.h"
+
 class Map
 {
 	public:
 		Map();
 		~Map();
 
-<<<<<<< HEAD
 		Locations grovnicks; // ex usage: grovnicks.get(1, 20);
 
-		void initializeMap(string fileName);
-=======
 		int initializeMap(string fileName);
->>>>>>> 0a9bd8bf868a06dcc1ea09d55a122714f14f1895
+
 		bool editTile(char tileType, int x, int y);
 
 		char map[128][128];
