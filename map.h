b63d478cbafdbe82d0420e47dfb82ee13c221b55
Gunnar R <gunnarrosenberg@gmail.com>
2020-11-25 15:05:43 -0800

    add grovnicks table to map

--- map.h
+++ map.h
@@ -1,5 +1,6 @@
 //map.h
 #include "locations.h"
+#include "grovnick.h"
 
 class Map
 {
@@ -15,5 +16,6 @@ class Map
 
 		char map[128][128];
 		char discovered[128][128];
+		Grovnick grovnicks[128][128];
 
 };
