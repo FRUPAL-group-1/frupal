7728ace39405038d8d6f71d3bfe257f7e5243a81
Gunnar R <gunnarrosenberg@gmail.com>
2020-12-02 15:07:41 -0800

    grovnicks loading progress

--- map.cpp
+++ map.cpp
@@ -49,4 +49,66 @@ bool Map::editTile(char tileType, int x, int y)
   return true;
 }
 
+
+// csv format
+// type, x, y, cost, name :: additional fields specific to type
+/*
+int Map::initializeGrovnicks()
+{
+
+  ifstream in;
+  in.open("grovnicks.csv");
+
+  while (in.good()){
+    string line, cell, type;
+    std::vector<string> values;
+
+    getline (in, line);
+    stringstream s(line);
+
+    while(s.good()) {
+      getline(s, col, ',');
+      values.push_back(cell);
+    }
+
+    switch(values[0]) {
+      case "food":
+        // code block
+        int y = s_toi(values[2]);
+        int x = s_toi(values[1]);
+        int cst = s_toi(values[3]);
+        int rstre = s_toi(values[5]);
+
+        Grovnick * ptr = new Food(y, x, cst, 1, values[4], rstre, false);
+
+        map[x][y] = ptr;
+
+        break;
+      case "obstacle":
+        // code block
+        break;
+      case "obstacle":
+        // code block
+        break;
+      case "tool":
+        // code block
+        break;
+      case "royal_diamond":
+        // code block
+        break;
+      case "clue":
+        // code block
+        break;
+      case "treasure":
+        // code block
+        break;
+      default:
+        // skip
+    }
+
+  }
+
+}
+*/
+
 #endif //MAP_CPP
